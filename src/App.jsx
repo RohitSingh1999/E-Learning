98f0bbe12d0335736c881be4c59ba2884b594adb
RohitSingh1999 <rohitsingh2371999@gmail.com>
2024-06-27 16:52:21 +0530

    update navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './common/Navbar'
+import Hero from './Hero'
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <Hero/>
+   
+   </>
+  )
+}
+
+export default App
---
+++ src/Hero.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function Hero() {
+  return (
+   <>
+
+<div class="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
+  <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+    
+    <div class="flex justify-center">
+      <a class="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-sm text-gray-800 p-1 ps-3 rounded-full transition hover:border-gray-300 dark:bg-neutral-800 dark:border-neutral-700 dark:hover:border-neutral-600 dark:text-neutral-200 font-bold" href="#">
+        Tutorial Point
+        <span class="py-1.5 px-2.5 inline-flex justify-center items-center gap-x-2 rounded-full bg-gray-200 font-semibold text-sm text-gray-600 dark:bg-neutral-700 dark:text-neutral-400">
+          <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+        </span>
+      </a>
+    </div>
+   
+
+
+    <div class="mt-5 max-w-2xl text-center mx-auto">
+      <h1 class="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-neutral-200">
+        E -{'\u00A0'}
+        <span class="bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent">Learning </span>
+      </h1>
+    </div>
+   
+
+    <div class="mt-5 max-w-3xl text-center mx-auto">
+      <p class="text-lg text-gray-600 dark:text-neutral-400 font-bold">This is a programming language tutorial in this tutorial you can learn more programming language in easy way</p>
+    </div>
+
+    <div class="mt-8 gap-3 flex justify-center">
+      <a class="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-4 dark:focus:ring-offset-gray-800" href="#">
+        Get started
+        <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+      </a>
+     
+    </div>
+ 
+
+   
+  </div>
+</div>
+
+
+   </>
+  )
+}
+
+export default Hero
